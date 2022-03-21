f119605d8835b1b260f447fe6e570a3d7c5be164
jtketz <50502062+jtketz@users.noreply.github.com>
2022-03-20 23:03:22 -0400

    Delete choice.m

--- choice.m
+++
@@ -1,9 +0,0 @@
-today = ['It is binary, make the choice: Easy or Hard...? '];
-choice = input(today);
-if choice == 0
-    disp('Easy choices lead to a hard life.')
-elseif choice == 1
-    disp('Hard choices lead to an easy life.')
-else
-    disp('Its binary, make the choice.')
-end
